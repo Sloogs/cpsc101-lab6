8da2e35a8b8f226959573a5c86bac62e829c80ab
Nick Slugocki <slugoc0@unbc.ca>
2020-02-25 16:56:38 -0800

    Add second button

--- ButtonViewer.java
+++ ButtonViewer.java
@@ -2,21 +2,37 @@ import java.awt.event.ActionListener;
 import java.awt.event.ActionEvent;
 import javax.swing.JButton;
 import javax.swing.JFrame;
+import javax.swing.JPanel;
 
 public class ButtonViewer
 {
-	private static final int FRAME_WIDTH = 100;
-	private static final int FRAME_HEIGHT = 60;
+	private static final int FRAME_WIDTH = 300;
+	private static final int FRAME_HEIGHT = 200;
+	private static int buttonAClickCount = 0;
+	private static int buttonBClickCount = 0;
 
 	public static void main(String[] args)
 	{
 		JFrame frame = new JFrame();
-		JButton button = new JButton("Click me!");
-		frame.add(button);
+		JPanel panel = new JPanel();
+		JButton buttonA = new JButton("Button A");
+		JButton buttonB = new JButton("Button B");
+		panel.add(buttonA);
+		panel.add(buttonB);
+		panel.setLocation(100,100);
+		frame.add(panel);
 
-		button.addActionListener(
+		buttonA.addActionListener(
 			(ActionEvent ae) -> {
-				System.out.println("I was clicked");
+				buttonAClickCount++;
+				System.out.println("Button A was clicked " + buttonAClickCount + " times!");
+			}
+		);
+
+		buttonB.addActionListener(
+			(ActionEvent ae) -> {
+				buttonBClickCount++;
+				System.out.println("Button B was clicked " + buttonBClickCount + " times!");
 			}
 		);
 
